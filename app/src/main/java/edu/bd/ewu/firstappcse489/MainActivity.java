78982bac20fbfb1c18b0017a1ae272da9b6db6df
faysalewucse <faysal.ewucse@gmail.com>
2022-06-19 22:53:26 +0600

    first commit

---
+++ app/src/main/java/edu/bd/ewu/firstappcse489/MainActivity.java
@@ -0,0 +1,147 @@
+package edu.bd.ewu.firstappcse489;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    String s, t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        s = "";
+        findViewById(R.id.one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"1";
+                result.setText(s);
+            }
+        });
+
+        findViewById(R.id.two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"2";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.three).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"3";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.four).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"4";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.five).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"5";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.six).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"6";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.seven).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"7";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.eight).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"8";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.nine).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"9";
+                result.setText(s);
+            }
+        });
+
+        findViewById(R.id.dot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+".";
+                result.setText(s);
+            }
+        });findViewById(R.id.zero).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"0";
+                result.setText(s);
+            }
+        });findViewById(R.id.plus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"+";
+                result.setText(s);
+            }
+        });findViewById(R.id.minus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"-";
+                result.setText(s);
+            }
+        });findViewById(R.id.div).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"÷";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.multi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"x";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.multi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = s+"x";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s = "";
+                result.setText(s);
+            }
+        });
+        findViewById(R.id.equal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
